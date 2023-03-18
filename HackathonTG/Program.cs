fc0395f63cf7d69780a3265d3b63701eb69fb6d4
Manuel Schumacher <manuel.schumacher@pebe.ch>
2023-03-18 11:10:26 +0100

    Init MudBlazor

--- HackathonTG/Program.cs
+++ HackathonTG/Program.cs
@@ -1,40 +1,31 @@
-using HackathonTG.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using pebe.Dashboard;
 
 namespace HackathonTG
 {
-	public class Program
+	public static class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
-			var builder = WebApplication.CreateBuilder(args);
-
-			// Add services to the container.
-			builder.Services.AddRazorPages();
-			builder.Services.AddServerSideBlazor();
-			builder.Services.AddSingleton<WeatherForecastService>();
-
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
-
-			app.UseHttpsRedirection();
-
-			app.UseStaticFiles();
-
-			app.UseRouting();
-
-			app.MapBlazorHub();
-			app.MapFallbackToPage("/_Host");
-
-			app.Run();
+			IHost webHost = CreateHostBuilder(args).Build();
+			await webHost.RunAsync();
 		}
+
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+			.ConfigureAppConfiguration(builder =>
+			{
+				builder.AddJsonFile("appsettings.json", false, true);
+				// Konfiguration pro Arbeitsstation zulassen
+				builder.AddJsonFile($"appsettings.{Environment.MachineName}.json", true, true);
+			})
+			.ConfigureWebHostDefaults(webBuilder =>
+			{
+				// DevExpress
+				webBuilder.UseWebRoot("wwwroot");
+				webBuilder.UseStaticWebAssets();
+
+				webBuilder.UseStartup<Startup>();
+			});
 	}
 }
---
+++ HackathonTG/Startup.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using MudBlazor.Services;
+
+namespace pebe.Dashboard
+{
+	/// <summary>
+	/// This methods gets called by the runtime. Use this methods to configure the HTTP request pipeline.
+	/// </summary>
+	public class Startup
+	{
+		public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+		{
+			Configuration = configuration;
+			Environment = environment;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		public IWebHostEnvironment Environment { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Blazor
+			services.AddRazorPages();
+			services.AddServerSideBlazor().AddCircuitOptions(o => o.DetailedErrors = !Environment.IsProduction());
+
+			// MudBlazor
+			services.AddMudServices();
+
+			// Services
+
+			// ViewModels
+
+			// Jobs
+		}
+
+		public void Configure(IApplicationBuilder app)
+		{
+			// Configure the HTTP request pipeline.
+			if (!Environment.IsProduction())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseRequestLocalization();
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				if (Environment.IsDevelopment())
+				{
+					endpoints.MapControllers().WithMetadata(new AllowAnonymousAttribute());
+				}
+				else
+				{
+					endpoints.MapControllers();
+				}
+
+				endpoints.MapBlazorHub();
+				endpoints.MapFallbackToPage("/_Host");
+			});
+		}
+	}
+}
